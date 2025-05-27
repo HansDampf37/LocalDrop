a54043fe2a8f32b3ef9c52bc39c6498c00e9c0c0
Adrian Degenkolb <adrian@degenkolb.de>
2025-05-27 17:34:41 +0300

    improve Navbar

--- src/main/java/org/deg/ui/NetworkTransferUI.java
+++ src/main/java/org/deg/ui/NetworkTransferUI.java
@@ -5,6 +5,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -20,10 +22,11 @@ public class NetworkTransferUI extends Application {
     private StackPane mainContent;
     private Pane receiveView;
     private Pane sendView;
-    private Backend backend = new Backend();
+    private final Backend backend = new Backend();
+    private Button btnReceive;
+    private Button btnSend;
 
-    public NetworkTransferUI() throws IOException {
-    }
+    public NetworkTransferUI() throws IOException {}
 
     public static void main(String[] args) {
         launch(args);
@@ -40,7 +43,7 @@ public class NetworkTransferUI extends Application {
         mainContent = new StackPane();
         receiveView = createReceiveView();
         sendView = createSendView();
-        mainContent.getChildren().add(receiveView);
+        loadSendPage();
 
         root.setCenter(mainContent);
 
@@ -55,19 +58,55 @@ public class NetworkTransferUI extends Application {
         VBox navBar = new VBox();
         navBar.setPadding(new Insets(10));
         navBar.setSpacing(10);
-        navBar.setPrefWidth(100);
+        navBar.setStyle("-fx-background-color: #d4edc9;");
+        navBar.setPrefWidth(200);
 
-        Button btnReceive = new Button("Receive");
-        Button btnSend = new Button("Send");
+        btnReceive = new Button("Receive");
+        btnSend = new Button("Send");
+
+        btnReceive.getStyleClass().add("nav-button");
+        btnSend.getStyleClass().add("nav-button");
 
         btnReceive.setMaxWidth(Double.MAX_VALUE);
         btnSend.setMaxWidth(Double.MAX_VALUE);
+        btnReceive.setMinWidth(150.0);
+        btnReceive.setMinHeight(75.0);
+        btnSend.setMinWidth(150.0);
+        btnSend.setMinHeight(75.0);
 
-        btnReceive.setOnAction(e -> mainContent.getChildren().setAll(receiveView));
-        btnSend.setOnAction(e -> mainContent.getChildren().setAll(sendView));
+
+        ImageView receiveIcon = new ImageView(new Image(getClass().getResource("/icons/user.png").toExternalForm()));
+        receiveIcon.setFitWidth(16);
+        receiveIcon.setFitHeight(16);
+
+        ImageView sendIcon = new ImageView(new Image(getClass().getResource("/icons/send.png").toExternalForm()));
+        sendIcon.setFitWidth(16);
+        sendIcon.setFitHeight(16);
+
+        // Set icons on buttons
+        btnReceive.setGraphic(receiveIcon);
+        btnSend.setGraphic(sendIcon);
+
+        btnReceive.setContentDisplay(ContentDisplay.LEFT);
+        btnSend.setContentDisplay(ContentDisplay.LEFT);
+
+        btnReceive.setOnAction(e -> loadReceivePage());
+        btnSend.setOnAction(e -> loadSendPage());
 
         navBar.getChildren().addAll(btnReceive, btnSend);
         return navBar;
+    }
+
+    private void loadReceivePage() {
+        btnReceive.getStyleClass().add("active");
+        btnSend.getStyleClass().remove("active");
+        mainContent.getChildren().setAll(receiveView);
+    }
+
+    private void loadSendPage() {
+        btnReceive.getStyleClass().remove("active");
+        btnSend.getStyleClass().add("active");
+        mainContent.getChildren().setAll(sendView);
     }
 
     private Pane createReceiveView() {
@@ -76,19 +115,19 @@ public class NetworkTransferUI extends Application {
 
         Label visibleAsLabel = new Label("You are visible as:");
 
-        ImageView profile_pic = new ImageView(new Image("https://picsum.photos/200"));
-        profile_pic.setFitWidth(200);
-        profile_pic.setFitHeight(200);
+        ImageView profilePic = new ImageView(new Image("https://picsum.photos/200"));
+        profilePic.setFitWidth(200);
+        profilePic.setFitHeight(200);
         Circle clip = new Circle(100, 100, 100);
-        profile_pic.setClip(clip);
+        profilePic.setClip(clip);
 
         Label nameLabel = new Label(backend.localPeer.name());
-        nameLabel.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
+        nameLabel.getStyleClass().add("nameLabel");
 
         Label ipLabel = new Label("IP: " + backend.localPeer.ip());
         Label portLabel = new Label("PORT: " + backend.localPeer.fileTransferPort());
 
-        box.getChildren().addAll(visibleAsLabel, profile_pic, nameLabel, ipLabel, portLabel);
+        box.getChildren().addAll(visibleAsLabel, profilePic, nameLabel, ipLabel, portLabel);
         return box;
     }
 
@@ -104,6 +143,9 @@ public class NetworkTransferUI extends Application {
         Button btnAddFile = new Button("Add File");
         Button btnAddFolder = new Button("Add Folder");
         Button btnText = new Button("Text");
+        btnAddFile.getStyleClass().add("btn");
+        btnAddFolder.getStyleClass().add("btn");
+        btnText.getStyleClass().add("btn");
         fileButtons.getChildren().addAll(btnAddFile, btnAddFolder, btnText);
 
         Label peersLabel = new Label("Peers");
@@ -134,7 +176,11 @@ public class NetworkTransferUI extends Application {
         layout.setAlignment(Pos.CENTER);
 
         Label title = new Label("Data received by Alice");
-        ImageView owlImage = new ImageView(new Image("https://picsum.photos/200"));
+        ImageView profilePic = new ImageView(new Image("https://picsum.photos/200"));
+        profilePic.setFitWidth(200);
+        profilePic.setFitHeight(200);
+        Circle clip = new Circle(100, 100, 100);
+        profilePic.setClip(clip);
         ListView<String> receivedFiles = new ListView<>();
         receivedFiles.getItems().addAll("Selected_file_1.txt", "Selected_file_2.txt");
 
@@ -148,7 +194,7 @@ public class NetworkTransferUI extends Application {
         buttons.setAlignment(Pos.CENTER);
         buttons.getChildren().addAll(abort, save);
 
-        layout.getChildren().addAll(title, owlImage, receivedFiles, buttons);
+        layout.getChildren().addAll(title, profilePic, receivedFiles, buttons);
 
         Scene scene = new Scene(layout, 300, 400);
         popup.setScene(scene);
