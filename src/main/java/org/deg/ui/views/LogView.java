1bdc6075c8c09da72ba7972e81c2e75df6bc5c23
Adrian Degenkolb <adrian@degenkolb.de>
2025-05-30 16:44:42 +0300

    Logs take up available space

--- src/main/java/org/deg/ui/views/LogView.java
+++ src/main/java/org/deg/ui/views/LogView.java
@@ -6,26 +6,24 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.util.Pair;
 import org.deg.backend.Backend;
 import org.deg.core.Peer;
 
 import java.io.File;
 
-public class LogView extends Pane {
+public class LogView extends VBox {
     public LogView(Backend backend) {
         HBox hbox = new HBox(15);
         hbox.setPadding(new Insets(15));
 
-        VBox vbox1 = new VBox();
+        // First column
+        VBox vbox1 = new VBox(5);
         Label labelSentLogs = new Label("Sent:");
         labelSentLogs.getStyleClass().add("h1");
         ListView<Pair<Peer, File>> sentLogs = new ListView<>();
-        ObservableList<Pair<Peer, File>> sentLogsList = FXCollections.observableArrayList();
-        sentLogsList.addAll(backend.getSentLog());
+        ObservableList<Pair<Peer, File>> sentLogsList = FXCollections.observableArrayList(backend.getSentLog());
         sentLogs.setItems(sentLogsList);
         sentLogs.setCellFactory(lv -> new ListCell<>() {
             @Override
@@ -34,14 +32,15 @@ public class LogView extends Pane {
                 setText(empty || item == null ? null : item.getValue().getName() + " to " + item.getKey().name());
             }
         });
+        VBox.setVgrow(sentLogs, Priority.ALWAYS);
         vbox1.getChildren().addAll(labelSentLogs, sentLogs);
 
-        VBox vbox2 = new VBox();
+        // Second column
+        VBox vbox2 = new VBox(5);
         Label labelReceivedLog = new Label("Received:");
         labelReceivedLog.getStyleClass().add("h1");
         ListView<Pair<Peer, File>> receivedLogs = new ListView<>();
-        ObservableList<Pair<Peer, File>> receivedLogsList = FXCollections.observableArrayList();
-        receivedLogsList.addAll(backend.getReceivedLog());
+        ObservableList<Pair<Peer, File>> receivedLogsList = FXCollections.observableArrayList(backend.getReceivedLog());
         receivedLogs.setItems(receivedLogsList);
         receivedLogs.setCellFactory(lv -> new ListCell<>() {
             @Override
@@ -50,9 +49,16 @@ public class LogView extends Pane {
                 setText(empty || item == null ? null : item.getValue().getName() + " from " + item.getKey().name());
             }
         });
+        VBox.setVgrow(receivedLogs, Priority.ALWAYS);
         vbox2.getChildren().addAll(labelReceivedLog, receivedLogs);
 
+        HBox.setHgrow(vbox1, Priority.ALWAYS);
+        HBox.setHgrow(vbox2, Priority.ALWAYS);
+
         hbox.getChildren().addAll(vbox1, vbox2);
+
+        // Allow hbox to grow
+        VBox.setVgrow(hbox, Priority.ALWAYS);
         getChildren().add(hbox);
     }
 }
